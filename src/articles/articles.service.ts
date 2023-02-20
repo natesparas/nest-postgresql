e048de3f04b35d1315b46c07445a20c22756635d
Jonathan Esparas <jonathan.esparas@stratpoint.com>
2023-02-20 10:42:34 +0000

    new changes

---
+++ src/articles/articles.service.ts
@@ -0,0 +1,48 @@
+// src/articles/articles.service.ts
+
+import { Injectable } from '@nestjs/common';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ArticlesService {
+  constructor(private prisma: PrismaService) {}
+
+  // CRUD operations
+  create(createArticleDto: CreateArticleDto) {
+    // return 'This action adds a new article';
+    return this.prisma.article.create({ data: createArticleDto });
+  }
+
+  findDrafts() {
+    return this.prisma.article.findMany({ where: { published: false } });
+  }
+
+  findAll() {
+    // return `This action returns all articles`;
+    return this.prisma.article.findMany();
+  }
+
+  findPublished() {
+    return this.prisma.article.findMany({ where: { published: true } });
+  }
+
+  findOne(id: number) {
+    // return `This action returns a #${id} article`;
+    return this.prisma.article.findUnique({ where: { id } });
+  }
+
+  update(id: number, updateArticleDto: UpdateArticleDto) {
+    // return `This action updates a #${id} article`;
+    return this.prisma.article.update({
+      where: { id },
+      data: updateArticleDto,
+    });
+  }
+
+  remove(id: number) {
+    // return `This action removes a #${id} article`;
+    return this.prisma.article.delete({ where: { id } });
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,45 @@
+// src/main.ts
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   const config = new DocumentBuilder()
+//     .setTitle('Median')
+//     .setDescription('The Median API description')
+//     .setVersion('0.1')
+//     .build();
+
+//   const document = SwaggerModule.createDocument(app, config);
+//   SwaggerModule.setup('api', app, document);
+
+//   await app.listen(3000);
+// }
+// bootstrap();
+
+import { NestFactory } from '@nestjs/core';
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
+import { AppModule } from './app.module';
+
+let server: Handler;
+
+async function bootstrap(): Promise<Handler> {
+  const app = await NestFactory.create(AppModule);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
+}
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
